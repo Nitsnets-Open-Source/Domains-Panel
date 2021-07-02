82f8fae93b75d8206835254427d65e058f7e54d6
Lito <lito@eordes.com>
2021-07-02 10:49:36 +0200

    Disable Robots

--- resources/views/layouts/molecules/head.blade.php
+++ resources/views/layouts/molecules/head.blade.php
@@ -1,6 +1,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<meta name="robots" content="none, noindex, nofollow, noarchive, nosnippet" />
+
 <title>{!! Meta::get('title') !!}</title>
 
 <meta name="csrf-token" content="{{ csrf_token() }}">
